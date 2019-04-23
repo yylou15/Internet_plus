d6d5d43e9c7230dc5b7eb6c965c2cb43eb5b61b2
Bruce <jzhengtao@outlook.com>
2019-04-23 15:54:53 +0800

    cancel the test

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("./view/login.fxml"));
-        primaryStage.setTitle("登录88886464646488");
+        primaryStage.setTitle("登录");
 
         primaryStage.setScene(new Scene(root, 300, 400));
         primaryStage.show();
